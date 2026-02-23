6c116890f0994dbbc60580c554c8cd44ea7770b9
Stepan <jakubec.stepik@gmail.com>
2026-02-23 20:48:52 +0100

    Testy + autor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        //author: Štěpán Jakubec
+        //github: https://github.com/Itzsteeeve/ETJ_IT_Project_2026/tree/master
+
         Hra hra = new Hra();
         hra.hraj();
     }
